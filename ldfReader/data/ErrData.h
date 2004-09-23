7dc46b74f5cb4890ba7a3a9edc77c83612e71fe0
heather <>
2004-09-23 05:16:56 +0000

    flag contributions when they actually exist in the event

--- ldfReader/data/ErrData.h
+++ ldfReader/data/ErrData.h
@@ -9,7 +9,7 @@ namespace ldfReader {
 
     /** @class ErrData
       * @brief Local storage of error data
-      * $Header: /nfs/slac/g/glast/ground/cvs/ldfReader/ldfReader/data/ErrData.h,v 1.1 2004/08/25 22:34:14 heather Exp $
+      * $Header: /nfs/slac/g/glast/ground/cvs/ldfReader/ldfReader/data/ErrData.h,v 1.2 2004/09/21 17:57:19 heather Exp $
     */
     class ErrData {
     public:
@@ -17,16 +17,21 @@ namespace ldfReader {
         ErrData() { clear(); };
         ErrData(const ErrData& error) { 
             m_summary = error.m_summary; 
-            m_lenInBytes = error.m_lenInBytes; };
+            m_lenInBytes = error.m_lenInBytes; 
+            m_exist = error.m_exist; };
         ErrData(const EventSummaryCommon &summary) { m_summary = summary; }
         ~ErrData() { clear(); };
 
         void clear() { 
             m_summary.setSummary(0); 
-            m_lenInBytes = 0; };
+            m_lenInBytes = 0; 
+            m_exist = false; };
 
         const EventSummaryCommon& summary() const { return m_summary; };
         void setSummary(unsigned summary) { m_summary.setSummary(summary);};
+
+        void setExist() { m_exist = true; };
+        bool exist() const { return m_exist; };
 
         void initLength(unsigned long length) { m_lenInBytes = length; };
         unsigned long lenInBytes() const { return m_lenInBytes; };
@@ -35,6 +40,8 @@ namespace ldfReader {
 
         // Store the event sequence number for this contribution
         EventSummaryCommon m_summary;
+        // denotes if this contribution exists for this event
+        bool m_exist;
 
         unsigned long m_lenInBytes;
     };
