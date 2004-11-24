0e9ff80fbcf59c6b1a86bbb9068c4cba063d56c0
heather <>
2004-11-24 21:45:06 +0000

    store tower and layer/gtcc for CAL/TKR diagnostic

--- ldfReader/data/TkrDiagnosticData.h
+++ ldfReader/data/TkrDiagnosticData.h
@@ -12,17 +12,22 @@ namespace ldfReader {
     {
     public:
 
-        TkrDiagnosticData(unsigned datum) : m_datum(datum) {}
+        TkrDiagnosticData(unsigned datum, unsigned tower, unsigned gtcc) : m_datum(datum),m_tower(tower),m_gtcc(gtcc) {}
 
         TkrDiagnosticData()  {}
         ~TkrDiagnosticData() {}
 
-        void init(unsigned data) { m_datum = data; };
+        void init(unsigned data, unsigned tower, unsigned gtcc) { 
+             m_datum = data; m_tower=tower; m_gtcc=gtcc; };
 
         unsigned dataWord() const { return m_datum;};
+        unsigned tower() const { return m_tower; };
+        unsigned gtcc() const { return m_gtcc; };
 
     private:
         unsigned m_datum;
+        unsigned m_tower;
+        unsigned m_gtcc;
     };
 } // end namespace
 #endif
--- src/iterators/DiagnosticParser.cxx
+++ src/iterators/DiagnosticParser.cxx
@@ -4,7 +4,7 @@
 /** @file DiagnosticParser.cxx
 @brief Implementation of the DiagnosticParser class
 
-$Header: /nfs/slac/g/glast/ground/cvs/ldfReader/src/iterators/DiagnosticParser.cxx,v 1.3 2004/02/05 17:29:51 heather Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ldfReader/src/iterators/DiagnosticParser.cxx,v 1.1.1.1 2004/04/15 20:02:22 heather Exp $
 */
 
 #include "DiagnosticParser.h"
@@ -29,7 +29,7 @@ namespace ldfReader {
         //     Order: 0(x0,x1) 2(x2,x3) 3(y0,y1) 4(y2,y3)
         //  const char*    layerLabel[] = {"x0", "x1", "x2", "x3", "y0", "y1", "y2", "y3"};
         //  const unsigned gccc[]       = {0, 2, 3, 1};
-        const ldfReader::CalDiagnosticData calDiagData(cal.datum());
+        const ldfReader::CalDiagnosticData calDiagData(cal.datum(), tower, layer);
         m_diagData->addCalDiagnostic(calDiagData);
         return 0;
     }
@@ -37,7 +37,7 @@ namespace ldfReader {
     int DiagnosticParser::TKRdiag(unsigned tower, unsigned gtcc,  TKRdiagnostic tkr)
     {
         // - 8 16 bit contributions for GTCC
-        const ldfReader::TkrDiagnosticData tkrDiagData(tkr.datum() & 0xffff);
+        const ldfReader::TkrDiagnosticData tkrDiagData(tkr.datum() & 0xffff, tower, gtcc);
         m_diagData->addTkrDiagnostic(tkrDiagData);
         return 0;
     }
