1de61ab04a01377d0b1573a87b0e436284ef47d0
heather <>
2006-03-31 01:36:20 +0000

    fix eventsummary storage for LICOS and readout4 check

--- src/iterators/CalParser.cxx
+++ src/iterators/CalParser.cxx
@@ -4,7 +4,7 @@
 /** @file CalParser.cxx
 @brief Implementation of the CalParser class
 
-$Header: /nfs/slac/g/glast/ground/cvs/ldfReader/src/iterators/CalParser.cxx,v 1.3 2004/07/21 05:09:26 heather Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ldfReader/src/iterators/CalParser.cxx,v 1.4 2005/04/05 21:23:39 heather Exp $
 */
 
 // EBF Online Library includes
@@ -89,7 +89,7 @@ namespace ldfReader {
         unsigned int offlineLayer = layer;
         unsigned int offlineColumn = theLog.column();
         // Determine the mode
-        unsigned readout4 = EventSummary::readout4(contribution()->summary());
+        unsigned readout4 = EventSummary::readout4(event()->summary());
         ldfReader::CalDigi::CalTrigMode mode = (readout4 == 0) ? ldfReader::CalDigi::BESTRANGE : ldfReader::CalDigi::ALLRANGE;
 
         // Retrieve the CalDigi object for this layer and column - or create a new one
