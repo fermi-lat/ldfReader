5c40c29456f11d8e3d67d7d2d6aa25d6053c1a32
heather <>
2006-08-08 17:04:02 +0000

    initial commit of socket handling on linux

---
+++ src/test/test_socketReader.cxx
@@ -0,0 +1,197 @@
+#ifndef TEST_socketReader_CXX
+#define TEST_socketReader_CXX 1
+
+
+/** @file test_socketReader.cxx
+@brief Test routine for the new EBF reader
+
+$Header: /nfs/slac/g/glast/ground/cvs/ldfReader/src/test/test_ldfReader.cxx,v 1.18 2006/08/01 15:52:16 heather Exp $
+*/
+
+#include <csignal>
+#include <iostream>
+
+#include "ldfReader/SocketParser.h"
+#include "facilities/Util.h"
+#include "../../src/EbfDebug.h" 
+#include "ldfReader/data/LatData.h"
+
+
+int main(int argn, char** argc) {
+  try {
+    using namespace ldfReader;
+    unsigned int server = 1;
+    std::string remap = "";
+
+
+    if( argn>1 ) server = atoi(argc[1]);
+    if( argn>2) remap = argc[2];
+    
+
+    EbfDebug::setDebug(true);
+
+    SocketParser *ebfP = new SocketParser(server);
+    ebfP->setAcdRemap(remap);
+
+    std::cout << "Setup socket" << std::endl;
+
+
+    int status = 0;
+    status = ebfP->nextEvent();
+/*
+    std::cout << "Calling next event" << std::endl;
+    do {
+        status = ebfP->nextEvent();
+
+    } while(status >= 0);
+ */
+
+    //ebfP->setIgnoreSegFault();
+    //int status = 0;
+    do {
+        status = ebfP->loadData();
+        if (status < 0) {
+            std::cout << "Failed to Load data - EOF" << std::endl;
+            break;
+        }
+        // Retrieve the LAT data for this event and print out its contents
+        LatData* myLatData = LatData::instance();
+
+        printf("\nSTART NEW EVENT\n");
+        printf("Run: %d, Event %d \n", myLatData->runId(), myLatData->summaryData().eventSequence());
+        printf("Event Size in Bytes %u \n", myLatData->eventSizeInBytes());
+
+        printf("EBF Format Identity 0x%08x\n", myLatData->getFormatIdentity());
+        // Event Summary
+        EventSummaryData summary = myLatData->summaryData();
+        summary.print();
+        printf("   Event GMT timestamp   = %ld.%09ld seconds after 1/1/1970\n",
+          summary.timeSec(), summary.timeNanoSec());
+
+        OswData osw = myLatData->getOsw();
+        osw.print();
+
+        // Check GEM data
+        GemData gem = myLatData->getGem();
+        gem.print();
+
+        AdfData adf = myLatData->getAdf();
+        adf.print();
+
+        AemData aem = myLatData->getAem(); 
+        aem.print();
+
+
+        // Check the ACD data
+        printf("ACD Data\n");
+        const std::map<const char*, AcdDigi*> acdCol = myLatData->getAcdCol();
+        std::map<const char*, AcdDigi*>::const_iterator acdIter;
+        for (acdIter = acdCol.begin(); acdIter != acdCol.end(); acdIter++) {
+            (acdIter->second)->print();
+        }
+
+        // Check the data in the Towers
+        unsigned int iTower;
+        for (iTower = 0; iTower < 16; iTower++) {
+            TowerData *tem = myLatData->getTower(iTower);
+            if (!tem) {
+                //printf("Tem does not exist\n");
+            } else {
+                tem->print();
+                printf("TEM:  %d\n", tem->getTowerId());
+                unsigned int i;
+                // Print CAL data
+                //const std::map<CalDigi> calData = tem->getCalDigiCol();
+                //std::vector<CalDigi>::const_iterator it;
+                printf("CAL data\n");
+
+                //      for (it = calData.begin(); it != calData.end(); it++) {
+                int iLayer, iCol;
+                const int maxLayerNum = 20;
+                const int maxColNum = 15;
+                for (iLayer = 0; iLayer < maxLayerNum; iLayer++) {
+                    for (iCol = 0; iCol < maxColNum; iCol++) {
+                        CalDigi* it = tem->getCalDigi(iLayer, iCol);
+                        if (!it) continue;
+                        printf("Layer = %d, Col = %d, Mode = %d\n", it->getLayerNum(), 
+                            it->getColumnNum(), it->getMode());
+                        CalDigi::CalTrigMode mode = it->getMode();
+                        if (mode == CalDigi::BESTRANGE) {
+                            const CalDigi::CalReadout *readout = it->getXtalReadout(0);
+                            printf("Pos Range %d, Pos ADC %d\n", readout->getRange(CalDigi::POS), readout->getAdc(CalDigi::POS));
+                            printf("Neg Range %d Neg ADC %d\n", readout->getRange(CalDigi::NEG), readout->getAdc(CalDigi::NEG));
+                        } else { // ALLRANGE
+                            unsigned int j;
+                            for (j=0; j<4; j++) {
+                                const CalDigi::CalReadout *readout = it->getXtalReadout(j);
+                                printf("Pos Range %d, Pos ADC %d\n", readout->getRange(CalDigi::POS), readout->getAdc(CalDigi::POS));
+                                printf("Neg Range %d Neg ADC %d\n", readout->getRange(CalDigi::NEG), readout->getAdc(CalDigi::NEG));
+
+                            }
+                        }
+                    }
+                }
+
+                // Print TKR data
+                printf("TKR Data\n");
+                for (i = 0; i < 18; i++) {
+                    TkrLayer *layer = tem->getTkrLayer(i);
+                    if (layer) {
+                        printf("    Layer:  %d\n", layer->getLayerNum());
+                        const std::vector<unsigned short> xStripColC0 = layer->getXStripC0Col();
+                        std::vector<unsigned short>::const_iterator xStripC0;
+                        printf("     X Strips C0: ");
+                        for (xStripC0 = xStripColC0.begin(); xStripC0 != xStripColC0.end(); xStripC0++) {
+                            printf(" %d ", *xStripC0);
+                        }
+                        printf("\n");
+                        const std::vector<unsigned short> xStripColC1 = layer->getXStripC1Col();
+                        std::vector<unsigned short>::const_iterator xStripC1;
+                        printf("     X Strips C1: ");
+                        for (xStripC1 = xStripColC1.begin(); xStripC1 != xStripColC1.end(); xStripC1++) {
+                            printf(" %d ", *xStripC1);
+                        }
+                        printf("\n");
+                        const std::vector<unsigned short> yStripColC0 = layer->getYStripC0Col();
+                        std::vector<unsigned short>::const_iterator yStripC0;
+                        printf("     Y Strips C0: ");
+                        for (yStripC0 = yStripColC0.begin(); yStripC0 != yStripColC0.end(); yStripC0++) {
+                            printf(" %d ", *yStripC0);
+                        }
+                        printf("\n");
+                        const std::vector<unsigned short> yStripColC1 = layer->getYStripC1Col();
+                        std::vector<unsigned short>::const_iterator yStripC1;
+                        printf("     Y Strips C1: ");
+                        for (yStripC1 = yStripColC1.begin(); yStripC1 != yStripColC1.end(); yStripC1++) {
+                            printf(" %d ", *yStripC1);
+                        }
+
+                        printf("\n");
+
+                    }
+                }
+                printf("\n\n");
+
+            }
+        }
+
+
+        printf("\nEND EVENT\n");
+        // Move the event pointer to the next event in the EBF file
+        status = ebfP->nextEvent();
+
+    } while (status >= 0);
+
+
+    delete ebfP;
+    ebfP = 0;
+
+    std::cout << "Test completed successfully" << std::endl;
+    return 0;
+  } catch(...) {
+    printf("Caught exception - exiting\n"); 
+    return(-1);
+  }
+}
+
+#endif
