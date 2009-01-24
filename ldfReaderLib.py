521e976b7cd266e28ff391495edc2be88d5478a5
ecephas <>
2009-01-24 17:46:06 +0000

    Added the following:
         env.Tool('addLibrary', library = env['ldfLibs'])
         env.Tool('facilitiesLib')
         env.Tool('eventFileLib')
         env.Tool('lsfDataLib')
         env.Tool('astroLib')

--- ldfReaderLib.py
+++ ldfReaderLib.py
@@ -1,12 +1,11 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ldfReader/ldfReaderLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/ldfReader/ldfReaderLib.py,v 1.2 2008/11/10 16:20:39 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'ldfReader')
+        env.Tool('addLibrary', library = ['ldfReader'])
     env.Tool('addLibrary', library = env['ldfLibs'])
-    env.Tool('addLibrary', library = env['cfitsioLibs'])
-    env.Tool('addLibrary', library = env['obfLibs'])
     env.Tool('facilitiesLib')
-    env.Tool('astroLib')
+    env.Tool('eventFileLib')
     env.Tool('lsfDataLib')
+    env.Tool('astroLib')
 def exists(env):
     return 1;
